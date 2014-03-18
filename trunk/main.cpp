b9dfdead6e2499f2aeffcf2b9a7a56fb791d2b5b
daniele.ronzani@gmail.com <daniele.ronzani@gmail.com@ca578f75-dfa6-0024-517d-875c626852a4>
2014-03-18 23:24:15 +0000

    primo caricamento file e mostro gli elementi

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,11 +1,39 @@
 // main.cpp
 // Il primo esempio in C++
 #include <iostream>
- 
+#include <fstream>
+#include <string.h>
+#include <vector>
 using namespace std;
+
 int main(int argc, char *argv[])
 {
-  cout << "Ciao Mondo" << endl;
- 
-  return 0;
+	ifstream file ("../bioinfo/illumina.sam");
+	char delim[] = "\t";
+	string line;
+	if(file.is_open())
+	{
+		int i = 0;
+		cout << "file letto e aperto" << endl;
+		while(getline (file,line) && i < 2)
+		{
+			cout << "\nLinea " << i << ": ";
+			cout << line << endl;
+			char * str = new char [line.length()+1];
+ 			strcpy (str, line.c_str());
+
+ 			str = strtok (str, delim);
+ 			
+ 			printf("Lista degli elementi:\n");
+		 	while(str != NULL)
+		 	{
+
+		 		printf("%s\n", str);
+		 		str = strtok (NULL, delim); 
+		 	}
+			i++;
+		}
+		file.close();
+	}
+	return 0;
 }
